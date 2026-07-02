076a367080f625a840995be2bc6faaedac311494
THXNXKXT <165754109+THXNXKXT@users.noreply.github.com>
2026-07-03 03:16:41 +0700

    fix: add ngrok domain to allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.1.183", "192.168.208.1"],
+  allowedDevOrigins: [
+    "192.168.1.183",
+    "192.168.208.1",
+    "anthology-moodiness-degraded.ngrok-free.dev",
+  ],
 };
 
 export default nextConfig;
